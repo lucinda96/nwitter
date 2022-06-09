a1403ac162f1de4379a734bd99573f4c321f1154
lucinda96 <lucinda96@naver.com>
2022-06-10 04:02:09 +0900

    1.1 chapter2 setting

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app';
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCJK4G0oI4hITNXNZErRNJDu4Ia_l2frB4",
+    authDomain: "nwitter-a8d06.firebaseapp.com",
+    projectId: "nwitter-a8d06",
+    storageBucket: "nwitter-a8d06.appspot.com",
+    messagingSenderId: "288136425013",
+    appId: "1:288136425013:web:f05db57346a97237590b7c"
+  };
+  export default initializeApp(firebaseConfig);
