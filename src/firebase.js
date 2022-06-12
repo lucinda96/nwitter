255480fa5174be7ef0d96f41402752ddd8d137f0
lucinda96 <lucinda96@naver.com>
2022-06-13 02:24:42 +0900

    1.3.1 chapter3 swtichErro

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from 'firebase/app';
 
   const firebaseConfig = {
-    apiKey: "AIzaSyCJK4G0oI4hITNXNZErRNJDu4Ia_l2frB4",
-    authDomain: "nwitter-a8d06.firebaseapp.com",
-    projectId: "nwitter-a8d06",
-    storageBucket: "nwitter-a8d06.appspot.com",
-    messagingSenderId: "288136425013",
-    appId: "1:288136425013:web:f05db57346a97237590b7c"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
   export default initializeApp(firebaseConfig);
